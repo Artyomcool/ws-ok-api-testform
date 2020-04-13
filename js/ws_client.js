953882e4329d265e0103c042431f6cb4ed82b458
Artyom Drozdov <artyomcool2@gmail.com>
2020-04-13 16:55:02 +0300

    Update ws_client.js

--- js/ws_client.js
+++ js/ws_client.js
@@ -29,43 +29,51 @@ function flushLog() {
 }
 
 function login(userName, pwd, sessionKey) {
+	if (document.getElementById("use_session").checked) {
+        createSession(sessionKey.trim());
+        return;
+	}
     OKSDK.REST.call("auth.login",
             {user_name: userName, password: pwd},
             function (status, data, error) {
-                var url = "ws" + getSslPrefix() + "://" + document.getElementById("api-host").value + "/websocket/" + document.getElementById("public-key").value;
-
-                if (document.getElementById("use_session").checked) {
-                    if (sessionKey.trim() !== "" && sessionKey.trim() !== "/") {
-                        url += "/";
-                    }
-                    url += sessionKey;
-                } else if (status === 'ok') {
-                    url += data['session_key'];
-                } else if (status !== 'ok') {
+                if (status !== 'ok') {
                     alert(OKSDK.Util.toString(error));
-                };
-                WS = new WebSocket(url);
-
-                WS.onopen = function(){
-                    document.getElementById("chatlog").textContent += "Connection opened\n";
-                };
-
-                WS.onmessage = function(message){
-                    document.getElementById("chatlog").textContent += "Got response:" + message.data + "\n";
-                    console.log("Got response: " + message.data);
-                };
-
-                WS.onerror = function (event) {
-                    document.getElementById("chatlog").textContent += "Got error:" + message.data + "\n";
-                };
-
-                WS.onclose = function (event) {
-                    document.getElementById("chatlog").textContent += "Connection closed\n";
-                };
+                } else {
+                    createSession(data['session_key']);
+                }
             },
             {no_session: true, app_secret_key: document.getElementById("secret-key").value}
     );
 }
+
+function createSession(sessionKey) {
+    var url = "ws" + getSslPrefix() + "://" + document.getElementById("api-host").value + "/websocket/" + document.getElementById("public-key").value;
+
+    if (sessionKey !== "" && !sessionKey.startsWith("/")) {
+        url += "/";
+    }
+    url += sessionKey;
+    
+    WS = new WebSocket(url);
+
+    WS.onopen = function(){
+        document.getElementById("chatlog").textContent += "Connection opened\n";
+    };
+
+    WS.onmessage = function(message){
+        document.getElementById("chatlog").textContent += "Got response:" + message.data + "\n";
+        console.log("Got response: " + message.data);
+    };
+
+    WS.onerror = function (event) {
+        document.getElementById("chatlog").textContent += "Got error:" + message.data + "\n";
+    };
+
+    WS.onclose = function (event) {
+        document.getElementById("chatlog").textContent += "Connection closed\n";
+    };
+}
+
 function getSslPrefix() {
     if (document.getElementById("ssl-enabled").checked) {
         return "s";
